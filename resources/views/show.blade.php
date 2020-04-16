240c8b825f0382f266110853dafa0ca1643fa797
sunsilver <jk05221@naver.com>
2020-04-16 21:38:32 +0900

    커뮤니티 상세 수정 삭제

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -6,16 +6,16 @@
 <body>
     <h1>글 상세페이지</h1>
     <div>
-        {{ $board->title }}
+        <h1>{{ $board->title }}</h1>
     </div>
     <div>
-        {{ $board->content }}
+        <p>{{ $board->content }}</p>
     </div>
  
     <div>
         
-    <!-- <button onclick="window.location='{{ url('/edit') }}/{{ $article->id }}'">수정</button>
-    <button onclick="window.location='{{ url('/delete') }}/{{ $article->id }}'">삭제</button> -->
+    <button onclick="window.location='{{ url('/edit') }}/{{ $board->id }}'">수정</button>
+    <button onclick="window.location='{{ url('/delete') }}/{{ $board->id }}'">삭제</button>
     <button onclick="window.location='{{ route('list') }}'">목록으로</button>
     </div>
  
