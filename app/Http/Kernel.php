61e9e4069a5fa56c55f3f39033d889918161d894
sunsilver <jk05221@naver.com>
2020-05-04 16:54:22 +0900

    cors2

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -14,6 +14,7 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $middleware = [
+        \App\Http\Middleware\CORS::class,
         \Fruitcake\Cors\HandleCors::class,
         \App\Http\Middleware\TrustProxies::class,
         \App\Http\Middleware\CheckForMaintenanceMode::class,
