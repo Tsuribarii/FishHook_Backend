5ed96b121ba710f6338b3f8ba0d444dc82772134
MAC <mac@MACui-MacBookPro.local>
2020-05-14 22:31:54 +0900

    update

--- app/Http/Controllers/FishingPlacesController.php
+++ app/Http/Controllers/FishingPlacesController.php
@@ -39,7 +39,7 @@ class FishingPlacesController extends Controller
             $items
         );
     }
-    public function paginate($items, $perPage = 9, $page = null, $options = []) { 
+    public function paginate($items, $perPage = 6, $page = null, $options = []) { 
         $page = $page ?: (Paginator::resolveCurrentPage() ?: 1); 
         $items = $items instanceof Collection ? $items : Collection::make($items); 
         return new LengthAwarePaginator($items->forPage($page, $perPage), $items->count(), $perPage, $page, $options); 
