eca7c9472164018da6a0ccfd97486242c756db5f
KIMJJUN <43381831+KIMJJUN@users.noreply.github.com>
2020-06-02 01:20:34 +0900

    image

---
+++ database/migrations/2020_06_01_161441_create_images_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateImagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('images', function(Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id')->comment("회원 번호");
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->string('fish_name')->comment("어종 이름");
+            $table->string('filename');
+            $table->string('url');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('images');
+    }
+}
