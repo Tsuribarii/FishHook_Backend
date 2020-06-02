f9b461d39b75632cd787edd35f795c9906d6c465
KIMJJUN <43381831+KIMJJUN@users.noreply.github.com>
2020-06-02 15:57:42 +0900

    api

--- routes/api.php
+++ routes/api.php
@@ -82,6 +82,9 @@ Route::get('/rank', 'RankController@rank');
 Route::delete('/rank/delete/{id}', 'RankController@destroy');
 Route::post('/image/store', 'ImageController@store');
 Route::post('/rank/store', 'RankController@store');
+Route::get('/image', 'ImageController@image');
+Route::get('/fishname', 'ImageController@fish_name');
+// Route::get('/rank/fish_name','RankController@fish_name');
 
 
 Route::get('/rank/fish_name','ImageController@fish_name');
