a8181908fac2373868c537b4f58b0ad145d1eebc
KIMJJUN <43381831+KIMJJUN@users.noreply.github.com>
2020-06-05 20:10:31 +0900

    image

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -24,11 +24,12 @@ class ImageController extends Controller
             $img
         );
     }
-    public function fish_name() {
-        $output = shell_exec("python /home/ubuntu/python/rockfish/rockfish/main.py");
+    public function fish_name(Request $request) {
+        $output = shell_exec("python3 /Users/gimjueun/Downloads/python/rockfish/rockfish/main.py");
         $a = strpos($output, '"');
         $result = substr($output,$a+1,-2);
-        return "볼락";
+        return $result;
+        
     }
 
     public function store(Request $request){  
@@ -44,13 +45,13 @@ class ImageController extends Controller
             $url = Storage::disk('s3')->url($path);
             $imagepath = 'https://awsfishhook.s3.ap-northeast-2.amazonaws.com/image/' .$name;
          }
-        //  $fish_name = $this -> fish_name();
-        //  $user = JWTAuth::parseToken()->authenticate();
-        //  Image::create([
-            // 'user_id'   => $user->id,
-            // 'fish_name' => $fish_name,
-            // 'filename'   => $name,
-            // 'url' => $imagepath
-        //  ]);
+         $fish_name = $this -> fish_name();
+         $user = JWTAuth::parseToken()->authenticate();
+         Image::create([
+            'user_id'   => $user->id,
+            'fish_name' => $fish_name,
+            'filename'   => $name,
+            'url' => $imagepath
+         ]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -84,7 +84,7 @@ Route::post('/image/store', 'ImageController@store');
 Route::post('/rank/store', 'RankController@store');
 Route::get('/image', 'ImageController@image');
 Route::get('/fishname', 'ImageController@fish_name');
-// Route::get('/rank/fish_name','RankController@fish_name');
+// Route::get('/rank/fish_name','ImageController@fish_name');
 
 
 Route::get('/rank/fish_name','ImageController@fish_name');
