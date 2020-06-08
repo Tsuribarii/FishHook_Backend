87197942eda296134eec2fb2172b84cf0fa54702
KIMJJUN <43381831+KIMJJUN@users.noreply.github.com>
2020-06-08 16:13:59 +0900

    fishname

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -26,9 +26,7 @@ class ImageController extends Controller
     }
     //어종분석
     public function fish_name(Request $request) {
-    // $output = shell_exec("python3 /home/ubuntu/python/rockfish/main.py 2>&1");
-    $command = escapeshellcmd('home/ubuntu/python/rockfish/main.py 2>&1');
-    $output = shell_exec($command);
+    $output = shell_exec("python3 /home/ubuntu/python/rockfish/main.py 1 > /dev/null 2>&1 &");
     var_dump($output);
 	#$command = escapeshellcmd('/home/ubuntu/python/rockfish/aa.py');
         #$output = Shell_exec($command);
