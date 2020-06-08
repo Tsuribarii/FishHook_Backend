5fc9787c25223b3eb7b8fb689a5fd96ff8b48ba2
KIMJJUN <43381831+KIMJJUN@users.noreply.github.com>
2020-06-08 19:23:24 +0900

    Image

--- app/Image.php
+++ app/Image.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Image extends Model
 {
     protected $guarded = [];
+    public function users()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
