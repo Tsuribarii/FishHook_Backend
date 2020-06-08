f4f3351ee50562ed89b0e476328e8277187f7ccf
KIMJJUN <43381831+KIMJJUN@users.noreply.github.com>
2020-06-08 15:59:02 +0900

    image

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -29,7 +29,7 @@ class ImageController extends Controller
     // $output = shell_exec("python3 /home/ubuntu/python/rockfish/main.py 2>&1");
     $command = escapeshellcmd('home/ubuntu/python/rockfish/main.pyi');
     $output = shell_exec($command);
-    echo $output;
+    var_dump($output);
 	#$command = escapeshellcmd('/home/ubuntu/python/rockfish/aa.py');
         #$output = Shell_exec($command);
 	#$output = shell_exec("python3 --version");    
