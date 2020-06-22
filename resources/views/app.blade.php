58a662c13aa1e8d282171b401a8de85386b2231b
KIMJJUN <43381831+KIMJJUN@users.noreply.github.com>
2020-06-22 16:33:50 +0900

    test

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,7 +7,7 @@
 
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>Laravel12</title>
+    <title>Laravel</title>
 
     <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/@mdi/font@4.x/css/materialdesignicons.min.css" rel="stylesheet">
